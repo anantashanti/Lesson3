74b76318734639860582d07b7d775146a6e103ac
anantashanti <anantashanti108@gmail.com>
2025-12-17 13:10:55 +0800

    Index

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
+// git config --global user.email "test@gmail"
+// git config --global user.name "TestName"
 // git init
 // git add .
-// git commit --m "Init"
+// git commit -m "Init"
 // git push
+
+// const files = ["cat.jpg", "dog.png", "document.pdf"];
+
+// files.push("music.mp3");
+
+// let fuel = 100; // топливо
+
+//  while (fuel > 0) {
+//     console.log("Едем " + "Топливо осталось: " + fuel);
+//     fuel -= 10; // fuel = fuel - 10;
+//  }
+
+// console.log("Приехали");
+
+// for (СТАРТ; ФИНИШ; ШАГ);
+
+for (let i = 1; i <= 5; i = i++) {
+    console.log("Отжимание номер " + i);
+}
